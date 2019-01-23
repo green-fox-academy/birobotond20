68ebede43c9dded393012a1a84230b0be04e906d
Bíró Botond <biro.botond20@gmail.com>
2019-01-23 14:44:23 +0100

    Solutions for Week-02, day-03 exercises

--- week-02/day-03/PurpleSteps/src/PurpleSteps.java
+++ week-02/day-03/PurpleSteps/src/PurpleSteps.java
@@ -1,2 +1,47 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 public class PurpleSteps {
+    public static void mainDraw(Graphics graphics) {
+        // Reproduce this:
+        // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+
+        drawSquareDiagonal(10, 20, graphics);
+    }
+
+    private static void drawSquareDiagonal(int size, int times, Graphics graphics){
+
+        for (int i = 1; i < times; i++){
+            graphics.setColor(Color.decode("#b145f3"));
+            graphics.fillRect(i*10, i*10, size, size);
+            graphics.setColor(Color.BLACK);
+            graphics.drawRect(i*10, i*10, size, size);
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
 }
