e71cc7270b81373a312d13628f2e0b93d77e2f5c
Bíró Botond <biro.botond20@gmail.com>
2019-01-29 15:19:48 +0100

    Solutions for week-03, day-02 exercises

---
+++ week-03/day-02/CopyFile/src/CopyFile.java
@@ -0,0 +1,33 @@
+// Write a function that copies a file to an other
+// It should take the filenames as parameters
+// It should return a boolean that shows if the copy was successful
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CopyFile {
+
+    public static void main(String[] args) throws IOException {
+
+        Path pathToFile = Paths.get("my-txt-file.txt");
+        Files.write(pathToFile, Collections.singleton("alma\nkörte\nbarack\nszőlő\ndinnye"));
+        System.out.println(copyFiles("my-txt-file.txt", "copied-content-from-my-txt.txt"));
+    }
+
+    private static boolean copyFiles (String copyFrom, String copyTo){
+
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(copyFrom));
+            Files.write(Paths.get(copyTo), lines);
+            return true;
+        } catch (IOException e){
+            System.out.println("valami");
+            return false;
+        }
+    }
+}
