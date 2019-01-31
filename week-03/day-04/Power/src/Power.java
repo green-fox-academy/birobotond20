420df0ddd82193ef73b9e9a593eca9e1dd4bed50
Bíró Botond <biro.botond20@gmail.com>
2019-01-31 12:19:11 +0100

    solutions for week-03, day-04 exercises

--- week-03/day-04/Power/src/Power.java
+++ week-03/day-04/Power/src/Power.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+// Given base and n that are both 1 or more, compute recursively (no loops)
+// the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
 
 public class Power {
+
+    public static void main(String[] args) {
+
+        System.out.println(power(2, 10));
+    }
+
+    private static int power(int base, int power){
+
+        if (power == 0){
+            return 1;
+        } else {
+            return power(base, power - 1) * base;
+        }
+    }
 }
--- week-03/day-04/SumDigit/src/SumDigit.java
+++ week-03/day-04/SumDigit/src/SumDigit.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+// Given a non-negative int n, return the sum of its digits recursively (no loops).
+// Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while
+// divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
 
 public class SumDigit {
+
+    public static void main(String[] args) {
+
+        System.out.println(sumOfDigits(126));
+    }
+
+    private static int sumOfDigits(int n) {
+
+        if (n < 0){
+            n *=  -1;
+        }
+
+        if (n <= 0) {
+            return 0;
+        } else {
+            System.out.println(n);
+            return sumOfDigits(n / 10) + n % 10;
+        }
+    }
 }
