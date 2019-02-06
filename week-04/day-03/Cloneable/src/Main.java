07fd28052a006b243bb5454b83e735ca328df8fe
Bíró Botond <biro.botond20@gmail.com>
2019-02-06 11:55:46 +0100

    solution for week-04, day-03 Cloneable exercise

---
+++ week-04/day-03/Cloneable/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student student = new Student("John", 20, "male", "BME");
+        Student johnTheClone = (Student) student.clone();
+
+        System.out.println(student.hashCode());
+        System.out.println(johnTheClone.hashCode());
+    }
+}
