b93f1502a7ff97e0c9e460407e14d13852e2cdf4
Bíró Botond <biro.botond20@gmail.com>
2019-02-25 16:47:08 +0100

    solution for week-06, day-01 exercise Exercise6

---
+++ week-06/day-01/Exercise6/src/Exercise6.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Exercise6 {
+
+    public static void main(String[] args) {
+
+        // Write a Stream Expression to find the uppercase characters in a string!
+        IntStream stringCharacters = "UpperCaseCharacter".chars();
+
+        // Method 1.
+        /*stringCharacters
+                .filter(Character::isUpperCase)
+                .mapToObj(character -> Character.toString((char)character))
+                .forEach(System.out::println);*/
+
+        // Method 2.
+        String uppercaseCharacter =
+        stringCharacters
+                .filter(Character::isUpperCase)
+                .mapToObj(character -> Character.toString((char)character))
+                .collect(Collectors.joining());
+        System.out.println(uppercaseCharacter);
+    }
+}
