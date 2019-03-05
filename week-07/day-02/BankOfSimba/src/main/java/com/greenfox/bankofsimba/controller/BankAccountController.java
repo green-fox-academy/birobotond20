38300aebaa6fb27727ed9b8c33e37699a1030d7f
Bíró Botond <biro.botond20@gmail.com>
2019-03-05 14:29:32 +0100

    add decimals and Zebra currency type to balance

--- week-07/day-02/BankOfSimba/src/main/java/com/greenfox/bankofsimba/controller/BankAccountController.java
+++ week-07/day-02/BankOfSimba/src/main/java/com/greenfox/bankofsimba/controller/BankAccountController.java
@@ -1,13 +1,22 @@
 package com.greenfox.bankofsimba.controller;
 
+import com.greenfox.bankofsimba.model.BankAccount;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class BankAccountController {
 
+    BankAccount simbaBankAccount = new BankAccount("Simba", 2000, "lion");
+
     @GetMapping("/show")
-    public String showAccount(){
-        return "template";
+    public String showAccount(Model model){
+        model.addAttribute("bankaccount", simbaBankAccount);
+        return "base_template";
     }
+
+    @GetMapping("/html_text")
+    public String displayHtmlText(@ModelAttribute)
 }
