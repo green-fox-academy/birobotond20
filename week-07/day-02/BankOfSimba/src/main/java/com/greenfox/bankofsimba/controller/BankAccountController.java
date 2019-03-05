2d118f1303a0347f7b7199b80eedcc4e0a73d446
Bíró Botond <biro.botond20@gmail.com>
2019-03-05 16:26:33 +0100

    set field to private and refactor showAccount method

--- week-07/day-02/BankOfSimba/src/main/java/com/greenfox/bankofsimba/controller/BankAccountController.java
+++ week-07/day-02/BankOfSimba/src/main/java/com/greenfox/bankofsimba/controller/BankAccountController.java
@@ -4,19 +4,22 @@ import com.greenfox.bankofsimba.model.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class BankAccountController {
 
-    BankAccount simbaBankAccount = new BankAccount("Simba", 2000, "lion");
+    private BankAccount simbaBankAccount = new BankAccount("Simba", 2000, "lion");
 
     @GetMapping("/show")
     public String showAccount(Model model){
         model.addAttribute("bankaccount", simbaBankAccount);
-        return "base_template";
+        return "single_account";
     }
 
     @GetMapping("/html_text")
-    public String displayHtmlText(@ModelAttribute)
+    public String displayHtmlText(Model model){
+        model.addAttribute("emphasized_text", "<em>HTML</em>");
+        model.addAttribute("bold_text", "<b>Enjoy yourself!</b>");
+        return "htmlception";
+    }
 }
