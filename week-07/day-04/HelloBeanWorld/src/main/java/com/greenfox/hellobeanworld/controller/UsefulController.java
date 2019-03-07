c3a84b1f24537b6bd8dcc874dacca05b0d0206cd
Bíró Botond <biro.botond20@gmail.com>
2019-03-07 13:06:44 +0100

    add color link list to /useful endpoint

---
+++ week-07/day-04/HelloBeanWorld/src/main/java/com/greenfox/hellobeanworld/controller/UsefulController.java
@@ -0,0 +1,24 @@
+package com.greenfox.hellobeanworld.controller;
+
+import com.greenfox.hellobeanworld.model.UtilityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsefulController {
+
+    private UtilityService utilities;
+
+    @Autowired
+    public UsefulController(UtilityService utilities){
+        this.utilities = utilities;
+    }
+
+    @GetMapping("/useful")
+    public String displayUtilities(Model model){
+        model.addAttribute("listOfColors", this.utilities.getColorList());
+        return "links_to_utilities";
+    }
+}
---
+++ week-07/day-04/HelloBeanWorld/src/main/java/com/greenfox/hellobeanworld/model/UtilityService.java
@@ -0,0 +1,31 @@
+package com.greenfox.hellobeanworld.model;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class UtilityService {
+    ArrayList<String> colors;
+    Random random;
+
+    public UtilityService() {
+        colors = new ArrayList<>();
+        colors.add("red");
+        colors.add("blue");
+        colors.add("lime");
+        colors.add("orange");
+        colors.add("magenta");
+        random = new Random();
+    }
+
+    public String randomColor() {
+        return colors.get(random.nextInt(colors.size()));
+    }
+
+    public List<String> getColorList(){
+        return this.colors;
+    }
+}
