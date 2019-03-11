efa07e215752067a2936ac3396300df87042739f
Bíró Botond <biro.botond20@gmail.com>
2019-03-11 12:44:23 +0100

    create and initialize ConnectionWithMySql app

---
+++ week-08/day-01/ConnectionWithMySql/src/main/java/com/greenfoxacademy/connectionwithmysql/ConnectionWithMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.connectionwithmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionWithMySqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectionWithMySqlApplication.class, args);
+    }
+
+}
---
+++ week-08/day-01/ConnectionWithMySql/src/main/java/com/greenfoxacademy/connectionwithmysql/controller/TodoController.java
@@ -0,0 +1,27 @@
+package com.greenfoxacademy.listingtodos.controller;
+
+import com.greenfoxacademy.listingtodos.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/todo")
+public class TodoController {
+
+    private TodoRepository todoRepository;
+
+    @Autowired
+    public TodoController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    @GetMapping(value = {"", "/", "/list"})
+    public String list(Model model) {
+        model.addAttribute("todos", todoRepository.findAll());
+        return "todolist";
+    }
+}
