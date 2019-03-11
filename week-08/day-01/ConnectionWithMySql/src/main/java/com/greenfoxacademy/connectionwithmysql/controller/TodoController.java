c909345761c5a0121a66692d0654942e0e909e53
Bíró Botond <biro.botond20@gmail.com>
2019-03-11 16:35:20 +0100

    add delete logic to app

--- week-08/day-01/ConnectionWithMySql/src/main/java/com/greenfoxacademy/connectionwithmysql/controller/TodoController.java
+++ week-08/day-01/ConnectionWithMySql/src/main/java/com/greenfoxacademy/connectionwithmysql/controller/TodoController.java
@@ -5,10 +5,7 @@ import com.greenfoxacademy.connectionwithmysql.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -48,6 +45,12 @@ public class TodoController {
     @PostMapping(value = "/add")
     public String addNewTodoSubmit(Todo todo) {
         todoRepository.save(todo);
+        return "redirect:/todo/list";
+    }
+
+    @GetMapping(value = "/{id}/delete")
+    public String deleteTodo(@PathVariable long id){
+        todoRepository.deleteById(id);
         return "redirect:/todo/list";
     }
 }
