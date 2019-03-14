5afcbe0f419de5e088c1c08b7dfa3fc965b43f2d
Bíró Botond <biro.botond20@gmail.com>
2019-03-14 11:49:37 +0100

    create basic model class Post

---
+++ week-08/day-04/RedditApp/src/main/java/com/greenfoxacademy/redditapp/model/Post.java
@@ -0,0 +1,54 @@
+package com.greenfoxacademy.redditapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String url;
+    private int numberOfVotes;
+
+    public Post() {
+    }
+
+    public Post(String title, String url) {
+        this.title = title;
+        this.url = url;
+        this.numberOfVotes = 0;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getNumberOfVotes() {
+        return this.numberOfVotes;
+    }
+
+    public void setNumberOfVotes(int numberOfVotes) {
+        this.numberOfVotes = numberOfVotes;
+    }
+}
