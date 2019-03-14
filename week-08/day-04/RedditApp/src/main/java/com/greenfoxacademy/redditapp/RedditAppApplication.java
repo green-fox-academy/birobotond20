89ecc34209ccef1d6ac4932c044c455487d5bf9d
Bíró Botond <biro.botond20@gmail.com>
2019-03-14 11:35:44 +0100

    initial state, create Project

---
+++ week-08/day-04/RedditApp/src/main/java/com/greenfoxacademy/redditapp/RedditAppApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.redditapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditAppApplication.class, args);
+    }
+
+}
---
+++ week-08/day-04/RedditApp/src/test/java/com/greenfoxacademy/redditapp/RedditAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.redditapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedditAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
