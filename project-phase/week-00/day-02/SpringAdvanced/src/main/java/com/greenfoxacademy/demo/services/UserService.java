81709b712ca50e7f316fa880de3c30485b7b518c
Bíró Botond <biro.botond20@gmail.com>
2019-04-03 15:28:37 +0200

    add saveUser logic to UserService

--- project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/services/UserService.java
+++ project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/services/UserService.java
@@ -8,22 +8,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-    private UserRepository userRepository;
     public String saveMessage;
+    private UserRepository userRepository;
 
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public void saveNewUser(User newUser){
+    public void saveNewUser(User newUser) {
         this.saveMessage = "";
-        if (newUser == null){
-            this.saveMessage = "No User object received";
-            return;
-        } else if (!newUser.getUserName().isEmpty() && !newUser.getPassword().isEmpty()){
-            this.saveMessage = "No usern";
+        if (userDataNotPresent(newUser.getUserName())) {
+            this.saveMessage = "No username entered";
+        } else if (userDataNotPresent(newUser.getPassword())) {
+            this.saveMessage = "No password entered";
+        } else if (isInDatabase(newUser.getUserName())) {
+            this.saveMessage = "This username is already taken";
+        } else {
+            this.userRepository.save(newUser);
+            this.saveMessage = "Saved new user";
         }
-        this.userRepository.save(newUser);
+    }
+
+    private boolean userDataNotPresent(String userData) {
+        return userData == null || userData.isEmpty();
+    }
+
+    private boolean isInDatabase(String userName) {
+        return this.userRepository.findByUserName(userName) != null;
     }
 }
