4b1f9fbbb21edf971748c64951ff692483127f2c
Bíró Botond <biro.botond20@gmail.com>
2019-04-03 15:26:33 +0200

    change CrudRepository generic type from Integer to Long

--- project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/repositories/UserRepository.java
+++ project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/repositories/UserRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUserName(String userName);
 }
