3eacc7bc9e591dfbed4c61c067e3f0188173dfd3
Bíró Botond <biro.botond20@gmail.com>
2019-04-03 15:21:32 +0200

    add save user logic to user service

--- project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/services/UserService.java
+++ project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/services/UserService.java
@@ -1,4 +1,29 @@
 package com.greenfoxacademy.demo.services;
 
+import com.greenfoxacademy.demo.models.User;
+import com.greenfoxacademy.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private UserRepository userRepository;
+    public String saveMessage;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void saveNewUser(User newUser){
+        this.saveMessage = "";
+        if (newUser == null){
+            this.saveMessage = "No User object received";
+            return;
+        } else if (!newUser.getUserName().isEmpty() && !newUser.getPassword().isEmpty()){
+            this.saveMessage = "No usern";
+        }
+        this.userRepository.save(newUser);
+    }
 }
