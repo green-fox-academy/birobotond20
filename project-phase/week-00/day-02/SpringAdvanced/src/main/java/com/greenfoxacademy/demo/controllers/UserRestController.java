006e1abc8bec08b5755ad2a4c8c06449d963c2ae
Bíró Botond <biro.botond20@gmail.com>
2019-04-03 15:27:55 +0200

    add saveUser logic to UserController

--- project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/controllers/UserRestController.java
+++ project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/controllers/UserRestController.java
@@ -1,4 +1,42 @@
 package com.greenfoxacademy.demo.controllers;
 
+import com.greenfoxacademy.demo.models.User;
+import com.greenfoxacademy.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class UserRestController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(path = "/")
+    public Object returnGreetMessage() {
+        return "Hello. This is your greeting! Never get old!";
+    }
+
+    @PostMapping(path = "/user/add")
+    public Object saveUser(@RequestBody User newUser) {
+        Map<String, String> returnMessage = new HashMap<>();
+        if (newUser == null) {
+            returnMessage.put("message", "No User object received");
+            return new ResponseEntity(returnMessage, null, HttpStatus.NOT_ACCEPTABLE);
+        }
+        this.userService.saveNewUser(newUser);
+        returnMessage.put("message", this.userService.saveMessage);
+        return returnMessage;
+    }
 }
