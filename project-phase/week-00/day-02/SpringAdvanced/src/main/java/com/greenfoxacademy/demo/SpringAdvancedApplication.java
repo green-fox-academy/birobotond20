b5819af3d19e389f488b90f675b66b483b01b558
Bíró Botond <biro.botond20@gmail.com>
2019-04-03 10:46:18 +0200

    initialize project with User model and Environment variables set

---
+++ project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/SpringAdvancedApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvancedApplication.class, args);
+    }
+}
