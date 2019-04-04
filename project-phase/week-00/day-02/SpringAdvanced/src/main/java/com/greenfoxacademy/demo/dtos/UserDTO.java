e45032c5ab4bc4ff4365fbeacca35837e46702e0
Bíró Botond <biro.botond20@gmail.com>
2019-04-04 16:24:23 +0200

    create UserDTO class

--- project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/dtos/UserDTO.java
+++ project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/dtos/UserDTO.java
@@ -1,4 +1,21 @@
 package com.greenfoxacademy.demo.dtos;
 
 public class UserDTO {
+
+    private String userName;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
