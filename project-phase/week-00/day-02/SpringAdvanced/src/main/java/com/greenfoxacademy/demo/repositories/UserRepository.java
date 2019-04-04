aea963abf5ba33bbc66b522a9b2202741dbe21c3
Bíró Botond <biro.botond20@gmail.com>
2019-04-04 16:26:01 +0200

    remove getAllUsersUserName logic from UserRepository

--- project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/repositories/UserRepository.java
+++ project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/repositories/UserRepository.java
@@ -1,17 +1,11 @@
 package com.greenfoxacademy.demo.repositories;
 
 import com.greenfoxacademy.demo.models.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
     User findByUserName(String userName);
-
-    @Query("SELECT u.id, u.userName FROM User u")
-    List<User> getAllUsersUserName();
 }
