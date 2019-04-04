57b2daa8fcd07dc00a2f10fc0a56532982aee52b
Bíró Botond <biro.botond20@gmail.com>
2019-04-04 16:27:02 +0200

    add User entity to UseDTO mapping logic to UserService

--- project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/services/UserService.java
+++ project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/services/UserService.java
@@ -1,11 +1,15 @@
 package com.greenfoxacademy.demo.services;
 
+import com.greenfoxacademy.demo.dtos.UserDTO;
 import com.greenfoxacademy.demo.encryptions.PasswordEncrypter;
 import com.greenfoxacademy.demo.models.User;
 import com.greenfoxacademy.demo.repositories.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
 @Service
@@ -34,8 +38,12 @@ public class UserService {
         }
     }
 
-    public List<User> getUserList(){
-        return this.userRepository.getAllUsersUserName();
+    public List<UserDTO> getUserList() {
+        List<User> userList = (List<User>) this.userRepository.findAll();
+        if (!userList.isEmpty()) {
+            return mapUserListToDtoList(userList);
+        }
+        return null;
     }
 
     private boolean userDataNotPresent(String userData) {
@@ -44,5 +52,14 @@ public class UserService {
 
     private boolean isInDatabase(String userName) {
         return this.userRepository.findByUserName(userName) != null;
+    }
+
+    private List<UserDTO> mapUserListToDtoList(List<User> userList) {
+
+        // Create anonymous subclass of TypeToken class, pass List<UserDTO> as type parameter
+        // and get the captured type information that otherwise would be erased an runtime
+        Type listType = new TypeToken<List<UserDTO>>() {
+        }.getType();
+        return new ModelMapper().map(userList, listType);
     }
 }
