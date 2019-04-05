da612bdfd961f9938344f6f7341f2268b7104474
Bíró Botond <biro.botond20@gmail.com>
2019-04-05 16:40:51 +0200

    UserRepository extend JpaRepository instead of CrudRepository

--- project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/repositories/UserRepository.java
+++ project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/repositories/UserRepository.java
@@ -1,11 +1,11 @@
 package com.greenfoxacademy.demo.repositories;
 
 import com.greenfoxacademy.demo.models.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUserName(String userName);
 }
