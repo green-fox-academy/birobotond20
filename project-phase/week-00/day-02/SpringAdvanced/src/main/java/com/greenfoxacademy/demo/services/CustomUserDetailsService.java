8eceee0093c1b6ba87cc7228afefbac1c0ab9f44
Bíró Botond <biro.botond20@gmail.com>
2019-04-05 17:27:49 +0200

    create UserDetailsService class

--- project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/services/CustomUserDetailsService.java
+++ project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/services/CustomUserDetailsService.java
@@ -1,4 +1,16 @@
 package com.greenfoxacademy.demo.services;
 
-public class CustomUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    // Here we are going to authenticate and need to provide an object of type UserDetails
+    // Also, since we need to return an object of UserDetails, we need to create a model of type
+    // UserDetails. UserDetails is an interface and we need to implement that in the model
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
