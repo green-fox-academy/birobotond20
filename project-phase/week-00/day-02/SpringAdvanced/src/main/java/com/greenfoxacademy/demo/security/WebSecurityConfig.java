110318f722b9dc3bdd204dec82d10dceec5f3fca
Bíró Botond <biro.botond20@gmail.com>
2019-04-05 17:28:10 +0200

    unfinished WebSecurityConfig class

--- project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/security/WebSecurityConfig.java
+++ project-phase/week-00/day-02/SpringAdvanced/src/main/java/com/greenfoxacademy/demo/security/WebSecurityConfig.java
@@ -1,43 +1,28 @@
 package com.greenfoxacademy.demo.security;
 
-import org.springframework.context.annotation.Bean;
+import com.greenfoxacademy.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity  // enables spring security on our application
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+//Inject all the classes that we need for the Jpa repositories
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {   // override some default spring security configurations and append our implementation to it
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .authorizeRequests()
-                .antMatchers("/", "/user/register", "/user/list").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
-    }
+    @Autowired
+    private UserDetailsService userDetailsService;  // Since this is an interface we need to implement it
 
-    @Bean
+    // We need to tell spring to load the data from the database:
+    //      1. Connect to the database
+    //      2. And load the data
     @Override
-    public UserDetailsService userDetailsService(){
-        UserDetails userDetails =
-                User.withDefaultPasswordEncoder()
-                    .username("username")
-                    .password("password")
-                    .roles("USER")
-                    .build();
-        return new InMemoryUserDetailsManager();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService()
     }
 }
