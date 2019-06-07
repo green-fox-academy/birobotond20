99a349ad426726a15c12ce6b0942a9a77e257d76
Bíró Botond <biro.botond20@gmail.com>
2019-06-07 13:43:31 +0200

    chore: delete project-phase folder, kerdesek.txt

--- week-04/day-02/GardenApp/src/Plant.java
+++ week-04/day-02/GardenApp/src/Plant.java
@@ -1,42 +1,42 @@
 public class Plant {
 
-    double currentWaterAmount;
-    String color;
-    double minWaterLevel;
-    double waterAbsorption;
+  double currentWaterAmount;
+  String color;
+  double minWaterLevel;
+  double waterAbsorption;
 
-    public Plant(String color) {
-        this.color = color;
-        this.currentWaterAmount = 0;
-        this.minWaterLevel = 2;
-        this.waterAbsorption = 0.1;
-    }
+  public Plant(String color) {
+    this.color = color;
+    this.currentWaterAmount = 0;
+    this.minWaterLevel = 2;
+    this.waterAbsorption = 0.1;
+  }
 
-    public boolean needsWater(){
-        if (this.currentWaterAmount < minWaterLevel){
-            return true;
-        } else {
-            return false;
-        }
+  public boolean needsWater(){
+    if (this.currentWaterAmount < minWaterLevel){
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    public void absorbWater(int inputWaterAmount){
-        setCurrentWaterAmount(getCurrentWaterAmount() + inputWaterAmount * this.waterAbsorption);
-    }
+  public void absorbWater(int inputWaterAmount){
+    setCurrentWaterAmount(getCurrentWaterAmount() + inputWaterAmount * this.waterAbsorption);
+  }
 
-    public double getCurrentWaterAmount() {
-        return currentWaterAmount;
-    }
+  public double getCurrentWaterAmount() {
+    return currentWaterAmount;
+  }
 
-    public void setCurrentWaterAmount(double currentWaterAmount) {
-        this.currentWaterAmount = currentWaterAmount;
-    }
+  public void setCurrentWaterAmount(double currentWaterAmount) {
+    this.currentWaterAmount = currentWaterAmount;
+  }
 
-    public String getColor() {
-        return color;
-    }
+  public String getColor() {
+    return color;
+  }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
+  public void setColor(String color) {
+    this.color = color;
+  }
 }
